2f87ba5ac515ceee80fc33c355d692b105218fe4
Amanda457 <mandy96lomi@gmail.com>
2024-10-10 21:07:38 +0200

    create methods index and store on user controller

---
+++ prova-backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserRequest;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+
+    public function index()
+    {
+        $users = User::all();
+
+        if ($users->isEmpty()) {
+            return response()->json(['message' => 'No hi ha usuaris registrats'], 200);
+        }
+    }
+    
+    public function store(StoreUserRequest $request)
+    {
+        try{
+            $user = new User;
+
+             $user->nom = $request->input('nom');
+             $user->cognom = $request->input('cognom');
+             $user->telefon = $request->input('telefon');
+             $user->edat = $request->input('edat');
+             $user->email = $request->input('email');
+
+             $user->save();
+            return response()->json([
+            'message' => 'Usuari registrat correctament',
+            'usuari' => $user
+        ]);
+    } catch (\Exception $e) {
+        return response()->json([
+            'message' => 'Error al registrar usuari',
+            'error' => $e->getMessage(),
+        ], 500);
+    }
+}
+    
+    public function show(string $id)
+    {
+        //
+    }
+
+
+    public function update(UpdateUserRequest $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
