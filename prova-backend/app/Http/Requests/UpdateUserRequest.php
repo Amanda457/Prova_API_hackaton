0db31dddaf2872eb42cd92298cb77aeb836c8e35
Amanda457 <mandy96lomi@gmail.com>
2024-10-10 21:06:54 +0200

    create update request

---
+++ prova-backend/app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'nom' => 'required|string|min:2',
+            'cognom' => 'required|string|min:2',
+            'telefon' => 'required|digits:9',
+            'edat' => 'required|int|min:0',
+            'email' => [
+                'nullable', 
+                'email', 
+                'max:70', 
+                Rule::unique('users')->ignore($this->route('user'))
+                 ]
+    ];}
+}
