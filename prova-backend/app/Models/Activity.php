d4081e5f7d8bc7a3733984e07bc0c2a7c9ed85b3
Amanda457 <mandy96lomi@gmail.com>
2024-10-11 09:17:51 +0200

    definir relació activity amb user

--- prova-backend/app/Models/Activity.php
+++ prova-backend/app/Models/Activity.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Activity extends Model
 {
@@ -12,5 +13,8 @@ class Activity extends Model
     protected $table = 'activities';
     protected $fillable = ['nom', 'descripcio', 'capacitat_maxima'];
 
-   
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
